dcf0a92c87100c7603e1271e15cd5e020ab992d5
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2025-05-20 20:12:34 +0530

    changed the functions prototype

--- core/src/main.c
+++ core/src/main.c
@@ -8,15 +8,7 @@ void led_init(void) {
     GPIOC->CRH |=  GPIO_CRH_MODE13_1;                       // Output mode 2 MHz
 }
 
-void led_toggle(void) {
-    GPIOC->ODR ^= GPIO_ODR_ODR13;
-}
-
-void delay(volatile uint32_t count) {
-    while (count--);
-}
-
-void led_on_task(void *param) {
+void task_sensor_data(void *param) {
     (void)param;
     vTaskDelay(pdMS_TO_TICKS(1));
     while (1) {
@@ -25,18 +17,43 @@ void led_on_task(void *param) {
     }
 }
 
-void led_off_task(void *param) {
-    (void)param;
-    while (1) {
-        GPIOC->ODR |= GPIO_ODR_ODR13;
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
+void task_processor(void *param) {
+    while(1);
+}
+
+void task_alert(void *param) {
+    while(1);
+}
+
+void task_controller(void *param) {
+    while(1);
 }
 
 int main(void) {
     led_init();
-    xTaskCreate(led_on_task, "LED_ON", 128, NULL, 1, NULL);
-    xTaskCreate(led_off_task, "LED_OFF", 128, NULL, 1, NULL);
+
+    // Task to read the sensor and send
+    // it to the task_processor
+    xTaskCreate(task_sensor_data, "sensor_data", 128, NULL, 1, NULL);
+
+    // Receive data from task_sensor_data and 
+    // process it. And inform task_alert if the average
+    // of temp or light go beyond thresold value.
+    xTaskCreate(task_processor, "processor", 128, NULL, 1, NULL);
+
+    // alert immediately when by an LED and
+    // buzzer, log the information via UART
+    xTaskCreate(task_alert, "alert", 128, NULL, 1, NULL);
+
+    // Listen for an interrupt using a key
+    // and switch between power saving mode
+    // and normal mode. where the alert thread
+    // should not log via UART, read light sensor
+    // data only. the average should be taken from
+    // last 10 value and this is to be done from alert
+    // task. the task_processor should be suspended completely
+    xTaskCreate(task_controller, "controller", 128, NULL, 1, NULL);
+
     vTaskStartScheduler();
     while (1);  // Should never reach here
 }
